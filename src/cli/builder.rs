3f5b4746d7bf3e5ac07c0c1c17668227b4ddab32
Vinicch <vinicius.leme@bemobi.com>
2022-09-19 03:39:55 -0300

    updates packages

--- src/cli/builder.rs
+++ src/cli/builder.rs
@@ -1,6 +1,6 @@
 use clap::{App, Arg, SubCommand};
 
-pub fn build_app() -> App<'static, 'static> {
+pub fn build_app() -> App<'static> {
     let args = build_args();
     let subcommands = build_subcommands();
 
@@ -12,16 +12,16 @@ pub fn build_app() -> App<'static, 'static> {
         .subcommands(subcommands)
 }
 
-fn build_args() -> Vec<Arg<'static, 'static>> {
+fn build_args() -> Vec<Arg<'static>> {
     vec![Arg::with_name("address")
-        .short("a")
+        .short('a')
         .long("address")
         .help("Sets the Host Address that's going to be looked up")
         .takes_value(true)
         .required(true)]
 }
 
-fn build_subcommands() -> Vec<App<'static, 'static>> {
+fn build_subcommands() -> Vec<App<'static>> {
     vec![
         SubCommand::with_name("ip").about("Looks up the IP addresses for a particular host"),
         SubCommand::with_name("mx").about("Looks up the DNS MX records for a particular host"),
