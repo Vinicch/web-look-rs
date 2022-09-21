b789c0cd5284129095c149ec0c0e08c83aeee4de
Vinicch <vinicius.leme@bemobi.com>
2022-09-21 12:15:22 -0300

    idiomatic for loop

--- src/cli/lookup.rs
+++ src/cli/lookup.rs
@@ -10,23 +10,23 @@ pub fn lookup(matches: ArgMatches) {
         Some("ip") => {
             let response = resolver.lookup_ip(address).unwrap();
 
-            response
-                .iter()
-                .for_each(|ip| println!("{ip}"));
+            for ip in response {
+                println!("{ip}")
+            }
         }
         Some("mx") => {
             let response = resolver.mx_lookup(address).unwrap();
 
-            response
-                .iter()
-                .for_each(|record| println!("{record}"));
+            for record in response {
+                println!("{record}")
+            }
         }
         Some("ns") => {
             let response = resolver.ns_lookup(address).unwrap();
 
-            response
-                .iter()
-                .for_each(|name| println!("{name}"));
+            for name in response {
+                println!("{name}")
+            }
         }
         _ => println!("Please specify what you want to lookup (ip, mx, ns).\n\nFor more information try --help")
     }
